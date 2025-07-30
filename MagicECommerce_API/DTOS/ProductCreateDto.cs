f80349d7b647b8f88138833d4719563e01212949
kevinpham12188 <kevinpham7788@gmail.com>
2025-07-29 22:56:38 -0700

    Added ProductCreateDto and ProductResponseDto

---
+++ MagicECommerce_API/DTOS/ProductCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicECommerce_API.DTOS
+{
+    public class ProductCreateDto
+    {
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(0.01, 999999.99)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int StockQuantity { get; set; }
+
+        [Required]
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ MagicECommerce_API/DTOS/ProductResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MagicECommerce_API.DTOS
+{
+    public class ProductResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public Guid CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
