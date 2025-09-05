6b1198ed16df9e47de0e7a80e14c8385b7b2ae79
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-05 13:19:58 -0700

    Add ICouponService

---
+++ MagicECommerce_API/Services/Interfaces/ICouponService.cs
@@ -0,0 +1,18 @@
+﻿using MagicECommerce_API.DTOS.Request;
+using MagicECommerce_API.DTOS.Response;
+using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Services.Interfaces
+{
+    public interface ICouponService
+    {
+        Task<IEnumerable<CouponResponseDto>> GetAllCouponAsync();
+        Task<CouponResponseDto?> GetCouponByIdAsync(Guid id);
+        Task<CouponResponseDto?> GetCouponByCodeAsync(string code);
+        Task<CouponResponseDto> CreateCouponAsync(CouponRequestDto dto);
+        Task<CouponResponseDto?> UpdateCouponAsync(Guid id, CouponRequestDto dto);
+        Task<bool> DeleteCouponAsync(Guid id);
+        Task<bool> ValidateCouponAsync(string code);
+        Task<CouponResponseDto?> UseCouponAsync(string code);
+    }
+}
