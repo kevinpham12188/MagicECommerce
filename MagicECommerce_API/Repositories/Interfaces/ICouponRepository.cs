4713044fc21be775d11847be34da9b8b36c4a92a
KevinPGainwell <phu.pham@gainwelltechnologies.com>
2025-09-04 23:16:48 -0700

    Add ICouponRepository

---
+++ MagicECommerce_API/Repositories/Interfaces/ICouponRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Repositories.Interfaces
+{
+    public interface ICouponRepository
+    {
+        Task<IEnumerable<Coupon>> GetAllCouponsAsync();
+        Task<Coupon?> GetCouponByIdAsync(Guid id);
+        Task<Coupon?> GetCouponByCodeAsync(string code);
+        Task<Coupon> CreateCouponAsync(Coupon coupon);
+        Task<Coupon?> UpdateCouponAsync(Guid id, Coupon coupon);
+        Task<bool> DeleteCouponAsync(Guid id);
+    }
+}
