a361b5448d506f9dac4fc28f2a6d368dee4eff33
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-10 12:41:08 -0700

    Add ProductImage in database

--- MagicECommerce_API/Data/ApplicationDBContext.cs
+++ MagicECommerce_API/Data/ApplicationDBContext.cs
@@ -9,5 +9,6 @@ namespace MagicECommerce_API.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
     }
 }
---
+++ MagicECommerce_API/Migrations/20250910194025_AddProductImages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicECommerce_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    AltText = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductImages");
+        }
+    }
+}
