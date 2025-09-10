e6ee411b25d70224ceae0838560b8dd6a9c96082
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-10 12:34:53 -0700

    Add ProductImageNotFoundException

---
+++ MagicECommerce_API/Exceptions/ProductImageException/ProductImageNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+using System.Net;
+
+namespace MagicECommerce_API.Exceptions.ProductImageException
+{
+    public class ProductImageNotFoundException : BaseCustomException
+    {
+        public ProductImageNotFoundException(Guid id) :
+         base($"Product not found", HttpStatusCode.NotFound)
+        { }
+    }
+}
