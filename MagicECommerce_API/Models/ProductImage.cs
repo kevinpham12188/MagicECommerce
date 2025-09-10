592029f849b08cfcc5e89f44c0bbc5ac945d51ec
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-10 10:20:29 -0700

    Add ProductImage model

---
+++ MagicECommerce_API/Models/ProductImage.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicECommerce_API.Models
+{
+    public class ProductImage
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Url { get; set; }
+
+        [MaxLength(255)]
+        public string AltText { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign key to Product
+        [Required]
+        public Guid ProductId { get; set; }
+    }
+}
