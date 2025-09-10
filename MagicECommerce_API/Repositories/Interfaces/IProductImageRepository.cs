05aa5b093145d8d8ef253b879a2b289000e8d6d1
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-10 15:46:50 -0700

    Add IProductImageRepository

---
+++ MagicECommerce_API/Repositories/Interfaces/IProductImageRepository.cs
@@ -0,0 +1,16 @@
+﻿using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Repositories.Interfaces
+{
+    public interface IProductImageRepository
+    {
+        Task<IEnumerable<ProductImage>> GetAllProductImageAsync();
+        Task<IEnumerable<ProductImage>> GetProductImagesByProductIdAsync(Guid productId);
+        Task<ProductImage?> GetProductImageByIdAsync(Guid id);
+        Task<ProductImage> CreateProductImageAsync(ProductImage productImage);
+        Task<ProductImage> UpdateProductImageAsync(ProductImage productImage);
+        Task<bool> DeleteProductImageAsync(Guid id);
+        Task<bool> DeleteProductImagesByProductIdAsync(Guid productId);
+        Task<bool> ProductExistsAsync(Guid productId);
+    }
+}
