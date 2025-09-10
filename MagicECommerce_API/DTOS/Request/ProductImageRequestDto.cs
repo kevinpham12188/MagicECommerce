ae1753e412dbb12613fd87606a64b6e3e7a132fb
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-10 12:23:43 -0700

    Add Product Request and Response Dto

---
+++ MagicECommerce_API/DTOS/Request/ProductImageRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicECommerce_API.DTOS.Request
+{
+    public class ProductImageRequestDto
+    {
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Url { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        public string? AltText { get; set; }
+
+    }
+}
---
+++ MagicECommerce_API/DTOS/Response/ProductImageResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicECommerce_API.DTOS.Response
+{
+    public class ProductImageResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string? AltText { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Guid ProductId { get; set; }
+    }
+}
