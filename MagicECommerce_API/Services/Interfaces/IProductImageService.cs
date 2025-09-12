c79164e878e35bf9fdf413ebfda3de75f6a44c92
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-12 13:20:35 -0700

    Add IProductImageService

---
+++ MagicECommerce_API/Services/Interfaces/IProductImageService.cs
@@ -0,0 +1,16 @@
+﻿using MagicECommerce_API.DTOS.Request;
+using MagicECommerce_API.DTOS.Response;
+
+namespace MagicECommerce_API.Services.Interfaces
+{
+    public interface IProductImageService
+    {
+        Task<IEnumerable<ProductImageResponseDto>> GetAllProductImageAsync();
+        Task<ProductImageResponseDto?> GetProductImageByIdAsync(Guid id);
+        Task<IEnumerable<ProductImageResponseDto>> GetProductImagesByProductIdAsync(Guid productId);
+        Task<ProductImageResponseDto> CreateProductImageAsync(ProductRequestDto dto);
+        Task<ProductImageResponseDto?> UpdateProductImageAsync(Guid id, ProductRequestDto dto);
+        Task<bool> DeleteProductImageAsync(Guid id);
+        Task<bool> DeleteProductImagesByProductIdAsync(Guid productId);
+    }
+}
