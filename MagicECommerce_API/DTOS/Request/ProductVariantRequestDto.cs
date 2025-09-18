a13ad0490d8e306983cf67ad56eda283a9503967
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-17 23:22:06 -0700

    Add DTOs for ProductVariant

---
+++ MagicECommerce_API/DTOS/Request/ProductVariantRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicECommerce_API.DTOS.Request
+{
+    public class ProductVariantRequestDto
+    {
+        public Guid ProductId { get; set; }
+        public string VariantName { get; set; } = string.Empty;
+        public string VariantValue { get; set; } = string.Empty;
+        public int StockQuantity { get; set; }
+    }
+}
---
+++ MagicECommerce_API/DTOS/Response/ProductVariantResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicECommerce_API.DTOS.Response
+{
+    public class ProductVariantResponseDto
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string VariantName { get; set; }
+        public string VariantValue { get; set; } 
+        public int StockQuantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
