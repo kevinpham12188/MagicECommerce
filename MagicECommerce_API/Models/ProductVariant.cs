6dec5d69404cf67a7b1d787475acc4514d3da1af
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-17 23:12:40 -0700

    Add Product Variant Model

---
+++ MagicECommerce_API/Models/ProductVariant.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicECommerce_API.Models
+{
+    public class ProductVariant
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid ProductId { get; set; }
+        [Required]
+        public string VariantName { get; set; } = string.Empty;
+        [Required]
+        public string VariantValue { get; set; } = string.Empty;
+        [Required]
+        public int StockQuantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public Product Product { get; set; }
+    }
+}
