b0aac1c3290a9396a7117a42d1b255d90030bb09
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-19 14:39:30 -0700

    Add IProductVariantRepository

---
+++ MagicECommerce_API/Repositories/Interfaces/IProductVariantRepository.cs
@@ -0,0 +1,19 @@
+﻿using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Repositories.Interfaces
+{
+    public interface IProductVariantRepository
+    {
+        Task<IEnumerable<ProductVariant>> GetAllVariantAsync();
+        Task<IEnumerable<ProductVariant>> GetVariantsByProductIdAsync(Guid productId);
+        Task<ProductVariant?> GetVariantByIdAsync(Guid id);
+        Task<ProductVariant?> CreateVariantAsync(ProductVariant variant);
+        Task<ProductVariant?> UpdateVariantAsycn(ProductVariant variant);
+        Task<bool> DeleteVariantAsync(Guid id);
+        Task<bool> DeleteVariantsByProductIdAsync(Guid productId);
+        Task<bool> VariantExistsAsync(Guid id);
+        Task<bool> ProductExistAsync(Guid productId);
+        Task<ProductVariant?> GetVariantByProductAndNameValueAsync(Guid productId, string name, string value);
+
+    }
+}
