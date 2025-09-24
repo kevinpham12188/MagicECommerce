95cfb23dbf0010651782dc43f9f990b4ca752001
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-23 22:30:27 -0700

    Add ProductVariantNotFoundException

---
+++ MagicECommerce_API/Exceptions/ProductVariantException/ProductVariantNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+using System.Net;
+
+namespace MagicECommerce_API.Exceptions.ProductVariantException
+{
+    public class ProductVariantNotFoundException : BaseCustomException
+    {
+        public ProductVariantNotFoundException(Guid id) :
+         base($"Product variant not found", HttpStatusCode.NotFound)
+        { }
+    }
+}
