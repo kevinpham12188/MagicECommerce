403cc7652fb8d8509bb88ea143c35cbfdfde692b
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-25 14:31:49 -0700

    Add IRoleService

---
+++ MagicECommerce_API/Services/Interfaces/IRoleService.cs
@@ -0,0 +1,16 @@
+﻿using MagicECommerce_API.DTOS.Request;
+using MagicECommerce_API.DTOS.Response;
+
+namespace MagicECommerce_API.Services.Interfaces
+{
+    public interface IRoleService
+    {
+        Task<RoleResponseDto> CreateRoleAsync(RoleRequestDto dto);
+        Task<RoleResponseDto> GetRoleByIdAsync(Guid id);
+        Task<RoleResponseDto> GetRoleByNameAsync(string name);
+        Task<IEnumerable<RoleResponseDto>> GetAllRolesAsync();
+        Task<RoleResponseDto> UpdateRoleAsync(Guid id, RoleRequestDto dto);
+        Task<RoleResponseDto> DeleteRoleAsync(Guid id);
+
+    }
+}
