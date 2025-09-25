00e397f106d348a5c7bf5ea599d0e9e6ebd0a87c
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-25 08:28:31 -0700

    Add Role Dtos

---
+++ MagicECommerce_API/DTOS/Response/RoleResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicECommerce_API.DTOS.Response
+{
+    public class RoleResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
