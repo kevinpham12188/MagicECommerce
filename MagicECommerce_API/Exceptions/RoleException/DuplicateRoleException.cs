a84da55f18c3b39c0c4091e0fd3ac7417fefd44c
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-25 08:46:37 -0700

    Add Role Exception

---
+++ MagicECommerce_API/Exceptions/RoleException/DuplicateRoleException.cs
@@ -0,0 +1,11 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+
+namespace MagicECommerce_API.Exceptions.RoleException
+{
+    public class DuplicateRoleException : BaseCustomException
+    {
+        public DuplicateRoleException(string name) :
+         base($"Role with name '{name}' already exists", System.Net.HttpStatusCode.Conflict)
+        { }
+    }
+}
---
+++ MagicECommerce_API/Exceptions/RoleException/RoleNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+using System.Net;
+
+namespace MagicECommerce_API.Exceptions.RoleException
+{
+    public class RoleNotFoundException : BaseCustomException
+    {
+        public RoleNotFoundException(Guid id) :
+         base($"Role not found", HttpStatusCode.NotFound)
+        { }
+
+        public RoleNotFoundException(string name) :
+         base($"Role with name '{name}' not found", HttpStatusCode.NotFound)
+        { }
+    }
+}
