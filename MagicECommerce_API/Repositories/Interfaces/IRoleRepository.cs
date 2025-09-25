560457aa29d00df66786455bb4ccfc4b15b01653
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-25 13:56:48 -0700

    Add IRoleRepository

---
+++ MagicECommerce_API/Repositories/Interfaces/IRoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Repositories.Interfaces
+{
+    public interface IRoleRepository
+    {
+        Task<Role> CreateAsync(Role role);
+        Task<Role> GetByIdAsync(Guid id);
+        Task<Role> GetByNameAsync(string name);
+        Task<IEnumerable<Role>> GetAllAsync();
+        Task<Role> UpdateAsync(Role role);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> RoleExistsAsync(Guid id);
+        Task<bool> RoleNameExistsAsync(string name);
+        Task<bool> RoleNameExistsAsync(string name, Guid excludeId);
+    }
+}
