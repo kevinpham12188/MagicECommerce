2946eef18da28a2875ccf16c66e2192e9fd5aa17
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-26 11:35:06 -0700

    Add User Exception

---
+++ MagicECommerce_API/Exceptions/UserException/UserAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+
+namespace MagicECommerce_API.Exceptions.UserException
+{
+    public class UserAlreadyExistsException : BaseCustomException
+    {
+        public UserAlreadyExistsException(string email) : base($"User with Email {email} already exists.", System.Net.HttpStatusCode.Conflict)
+        {
+        }
+    }
+}
---
+++ MagicECommerce_API/Exceptions/UserException/UserNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+using System.Net;
+
+namespace MagicECommerce_API.Exceptions.UserException
+{
+    public class UserNotFoundException : BaseCustomException
+    {
+        public UserNotFoundException(Guid userId) : base($"User with ID {userId} was not found.", HttpStatusCode.NotFound)
+        {
+        }
+        public UserNotFoundException(string email) : base($"User with Email {email} was not found.", HttpStatusCode.NotFound)
+        {
+        }
+    }
+}
