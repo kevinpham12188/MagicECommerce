363a5e166ffd07bf05602d9480168ea52dd58272
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-26 11:55:59 -0700

    Add IUserRepository

---
+++ MagicECommerce_API/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<User?> GetByIdAsync(Guid id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByRoleAsync(Guid roleId);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<IEnumerable<User>> GetAllWithRolesAsync();
+        Task<IEnumerable<User>> GetUsersByRoleIdAsync(Guid roleId);
+        Task<User?> UpdateAsync(User user);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> UserExistsAsync(Guid id);
+        Task<bool> EmailExistsAsync(string email);
+        Task<bool> EmailExistsAsync(string email, Guid excludeUserId);
+    }
+}
