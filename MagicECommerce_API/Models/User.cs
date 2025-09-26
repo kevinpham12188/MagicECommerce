eae86f74c27ed720f8d47b7c2e7cea6d9bdd6527
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-26 09:38:22 -0700

    Add User model

---
+++ MagicECommerce_API/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicECommerce_API.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        [Required]
+        public Guid RoleId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        [ForeignKey("RoleId")]
+        public Role? Role { get; set; }
+    }
+}
