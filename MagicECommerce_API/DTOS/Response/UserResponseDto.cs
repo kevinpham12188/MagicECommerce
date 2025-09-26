8293c0efde4fdd2bd0f0b66e4583cfc674f9802f
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-26 10:37:12 -0700

    Add User Dtos

---
+++ MagicECommerce_API/DTOS/Response/UserResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MagicECommerce_API.DTOS.Response
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public Guid RoleId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
