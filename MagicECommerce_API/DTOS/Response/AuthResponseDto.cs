3d59b29fe25a7de2fd2a531c0065ed720bf520c7
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-27 23:09:39 -0700

    Add AuthResponseDto

---
+++ MagicECommerce_API/DTOS/Response/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicECommerce_API.DTOS.Response
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+        public UserResponseDto User { get; set; } = new();
+    }
+}
