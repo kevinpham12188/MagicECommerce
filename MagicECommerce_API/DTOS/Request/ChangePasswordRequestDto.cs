6cc1ffab5f6072f5a0c793d5fbb7470a3b0f7a46
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-27 22:53:44 -0700

    Add ChangePasswordREquestDto

---
+++ MagicECommerce_API/DTOS/Request/ChangePasswordRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicECommerce_API.DTOS.Request
+{
+    public class ChangePasswordRequestDto
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
