5a512179c89b3f2edd1156e342cd162803320aaa
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-27 23:13:27 -0700

    Add ITokenService

---
+++ MagicECommerce_API/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using MagicECommerce_API.Models;
+using System.Security.Claims;
+
+namespace MagicECommerce_API.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(User user);
+        ClaimsPrincipal? ValidateToken(string token);
+    }
+}
