5e52b1f90b572e22a81082eb97dfc4f0a468e921
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-28 23:47:28 -0700

    Add IAuthService

---
+++ MagicECommerce_API/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using MagicECommerce_API.DTOS.Request;
+using MagicECommerce_API.DTOS.Response;
+
+namespace MagicECommerce_API.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> LoginAsync(LoginRequestDto dto);
+        Task<UserResponseDto> RegisterAsync(UserCreateRequestDto dto);
+        Task<bool> ChangePasswordAsyunc(Guid userId, ChangePasswordRequestDto dto);
+        Task<UserResponseDto> GetCurrentUserAsync(Guid userId);
+    }
+}
