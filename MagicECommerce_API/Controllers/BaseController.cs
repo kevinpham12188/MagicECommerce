946edff6eff9d7a17715fc2d40a948d252b357fd
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-29 15:13:46 -0700

    Add BaseController to find user info

---
+++ MagicECommerce_API/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MagicECommerce_API.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected Guid GetCurrentUserId()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Guid.TryParse(userIdClaim, out var userId) ? userId : Guid.Empty;
+        }
+
+        protected string GetCurrentUserEmail()
+        {
+            return User.FindFirst(ClaimTypes.Email)?.Value ?? string.Empty;
+        }
+
+        protected string GetCurrentUserRole()
+        {
+            return User.FindFirst(ClaimTypes.Role)?.Value ?? string.Empty;
+        }
+
+        protected bool IsInRole(params string[] roles)
+        {
+            var userRole = GetCurrentUserRole();
+            return roles.Contains(userRole);
+        }
+    }
+}
