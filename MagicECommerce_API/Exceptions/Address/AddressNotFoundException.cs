83f2d13116dbefe9a265cf299a88418fce561328
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-30 15:13:54 -0700

    Add AddressNotFoundException

---
+++ MagicECommerce_API/Exceptions/Address/AddressNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+using System.Net;
+
+namespace MagicECommerce_API.Exceptions.Address
+{
+    public class AddressNotFoundException : BaseCustomException
+    {
+        public AddressNotFoundException(Guid id) : base("Address not found.", HttpStatusCode.NotFound) { }
+    }
+}
