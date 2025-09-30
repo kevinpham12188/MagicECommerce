b7932f76d97a9d58141b0159a5e2d70424163949
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-29 23:24:17 -0700

    Add RequiredRoleAttribute to allow using RequireRole annotation

---
+++ MagicECommerce_API/Attributes/RequireRoleAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace MagicECommerce_API.Attributes
+{
+    public class RequireRoleAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string[] _role;
+        public RequireRoleAttribute(params string[] role)
+        {
+            _role = role;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // Check if user is authenticated
+            if (!context.HttpContext.User.Identity?.IsAuthenticated ?? true)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            // Get user's role
+            var userRole = context.HttpContext.User.FindFirst(ClaimTypes.Role)?.Value;
+            if (string.IsNullOrEmpty(userRole) || !_role.Contains(userRole))
+            {
+                context.Result = new ForbidResult();
+                return;
+            }
+        }
+    }
+}
