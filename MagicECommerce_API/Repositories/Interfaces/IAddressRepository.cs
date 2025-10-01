6eeb97312eb939382f0668460b0c4446cc9a34e9
KevinPGainwell <kevinpham7788@gmail.com>
2025-09-30 17:41:52 -0700

    Add IAddressrepository

---
+++ MagicECommerce_API/Repositories/Interfaces/IAddressRepository.cs
@@ -0,0 +1,17 @@
+﻿using MagicECommerce_API.Models;
+
+namespace MagicECommerce_API.Repositories.Interfaces
+{
+    public interface IAddressRepository
+    {
+        Task<Address> CreateAsync(Address address);
+        Task<Address?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Address>> GetByUserIdAsync(Guid userId);
+        Task<Address?> GetDefaultByUserIdAsync(Guid userId);
+        Task<Address> UpdateAsync(Address address);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+        Task<bool> UserOwnsAddressAsync(Guid userId, Guid addressId);
+        Task ClearDefaultForUserAsync(Guid userId);
+    }
+}
