253d0833b10a577f1fe57058a1b328e9d99342ac
KevinPGainwell <kevinpham7788@gmail.com>
2025-10-03 13:00:30 -0700

    Add ProductReviewNotFoundException

---
+++ MagicECommerce_API/Exceptions/ProductReviewException/ProductReviewNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using MagicECommerce_API.Exceptions.Base;
+using System.Net;
+
+namespace MagicECommerce_API.Exceptions.ProductReviewException
+{
+    public class ProductReviewNotFoundException : BaseCustomException
+    {
+        public ProductReviewNotFoundException(Guid id) : base("Product review not found.", HttpStatusCode.NotFound)
+        {
+        }
+    }
+}
