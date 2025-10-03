857ee88cf5e35fdac3587aa5b573895ddc0e4d58
KevinPGainwell <kevinpham7788@gmail.com>
2025-10-03 13:26:12 -0700

    Add IProductService and add ProductRatingStatsDto

---
+++ MagicECommerce_API/DTOS/Response/ProductRatingStatsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace MagicECommerce_API.DTOS.Response
+{
+    public class ProductRatingStatsDto
+    {
+        public Guid ProductId { get; set; }
+                public double AverageRating { get; set; }
+        public int TotalReviews { get; set; }
+        public Dictionary<int, int> RatingDistribution { get; set; } = new()
+        {
+            { 1, 0 },
+            { 2, 0 },
+            { 3, 0 },
+            { 4, 0 },
+            { 5, 0 }
+        };
+    }
+}
---
+++ MagicECommerce_API/Services/Interfaces/IProductReviewService.cs
@@ -0,0 +1,18 @@
+﻿using MagicECommerce_API.DTOS.Request;
+using MagicECommerce_API.DTOS.Response;
+
+namespace MagicECommerce_API.Services.Interfaces
+{
+    public interface IProductReviewService
+    {
+        Task<IEnumerable<ProductReviewResponseDto>> GetAllReviewsAsync();
+        Task<IEnumerable<ProductReviewResponseDto>> GetReviewsByProductIdAsync(Guid productId);
+        Task<IEnumerable<ProductReviewResponseDto>> GetReviewsByUserIdAsync(Guid userId);
+        Task<ProductReviewResponseDto?> GetReviewByIdAsync(Guid id);
+        Task<ProductReviewResponseDto> CreateReviewAsync(Guid userId, ProductReviewRequestDto reviewDto);
+        Task<ProductReviewResponseDto> UpdateReviewAsync(Guid id, Guid userId, ProductReviewRequestDto reviewDto);
+        Task<bool> DeleteReviewAsync(Guid id, Guid userId);
+        Task<bool> AnonymizeUserReviewsAsync(Guid userId);
+        Task<ProductRatingStatsDto> GetProductRatinStatsAsync(Guid productId);
+    }
+}
